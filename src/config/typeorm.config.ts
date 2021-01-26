13258a2c5b656444428059e3d26b6fd609d500ea
Markiyan Pyts <markiyan.pyts@osf-commerce.com>
2021-01-26 19:57:30 +0200

    Logging

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,15 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as config from 'config';
+
+const dbConfig = config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-    entities: [__dirname + '/../**/*.entity.{js,ts}'],
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'tifind96',
-    database: 'taskmanagement',
-    synchronize: true, //not reccomended to set to true in prod
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  type: dbConfig.type,
+  host: process.env.RDS_HOSTNAME || dbConfig.host,
+  port: process.env.RDS_PORT || dbConfig.port,
+  username: process.env.RDS_USERNAME || dbConfig.username,
+  password: process.env.RDS_PASSWORD || dbConfig.password,
+  database: process.env.RDS_DB_NAME || dbConfig.database,
+  synchronize: dbConfig.synchronize, //not reccomended to set to true in prod
 };
